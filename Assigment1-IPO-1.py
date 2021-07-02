aeaf12273e9a1f5ea12a7bbfc3ed595d2e974b0f
Danitza Fuenzalida <danitzafuenzalidabarriga@gmail.com>
2021-07-02 14:40:41 -0700

    Add files via upload

---
+++ Assigment1-IPO-1.py
@@ -0,0 +1,16 @@
+# Assignment 1-IPO-1, by Danitza Fuenzalida
+
+""" The following variables store the name and
+    address that the user gives when prompted.
+"""
+
+name = input("Hello, what is your name? ")
+address = input("Now, what is your address? ")
+
+""" The following lines print the given strings
+    in different lines, along with concatenating 
+    the variables into the strings. 
+"""
+print("We received the following information:")
+print("Your name is: " + name)
+print("Your address is: " + address)

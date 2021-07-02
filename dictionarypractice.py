599f598fce04f81191bee39e739cd6005be4733b
Danitza Fuenzalida <danitzafuenzalidabarriga@gmail.com>
2021-07-02 14:45:23 -0700

    Add files via upload

---
+++ dictionarypractice.py
@@ -0,0 +1,22 @@
+# Dictionary code practice
+def population_avg():
+
+    capitalsize = capitalsizedic()
+    capitalsize["WA"]["OH"]["PT"]["NM"]["IL"]
+
+
+# Main function, always required:
+def main():
+    # Creating a dictionary called capitalsize.
+    capitalsize = {
+        "WA": "7,705,281",
+        "OH": "11,799,448",
+        "PT": "3,285,874",
+        "NM": "2,117,522",
+        "IL": "12,812,508"
+    }
+    # Print dictionary just to check that everything works
+    print(captialsize)
+
+# Call main function
+main()
